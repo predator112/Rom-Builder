e017fa71d67cc601e10291088ae2dec1a74dfbc7
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2022-03-24 11:40:59 +0700

    Release VIP 🤩

--- crdroid.sh
+++ crdroid.sh
@@ -9,7 +9,7 @@ lunch lineage_rosy-userdebug
 export TZ=Asia/Jakarta
 export USE_NFSparts=true
 export NFS_BUILD_VARIANT=overclock
-curl -s -X POST "https://api.telegram.org/bot${tg_token}/sendMessage" -d chat_id="${tg_id}" -d "disable_web_page_preview=true" -d "parse_mode=html" -d text="===================================%0A<b>$device</b> Starting Building Rom%0A<b>$rom_name</b>%0A<b>VIP</b>$(echo "${var_cache_report_config}")"
+curl -s -X POST "https://api.telegram.org/bot${tg_token}/sendMessage" -d chat_id="${tg_id}" -d "disable_web_page_preview=true" -d "parse_mode=html" -d text="===================================%0A<b>$device</b> Starting Building Rom%0A<b>ROM:</b>$rom_name%0A<b>Build Type:</b>VIP%0A$(echo "${var_cache_report_config}")"
 brunch rosy
 
 # end
