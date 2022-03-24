bd5fc98e69b42c82d0216f346b369cdad93889f5
NFS86 <jarbull86@gmail.com>
2022-03-24 09:04:32 +0000

    Add build script CherishOS
    Signed-off-by: NFS86 <jarbull86@gmail.com>

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Builder with connecting to ssh server
 
 # Building Status In Progress
 [![Crdroid](https://github.com/NFS86/Rom-Builder/actions/workflows/crdroid.yml/badge.svg)](https://github.com/NFS86/Rom-Builder/actions/workflows/crdroid.yml)
+[![CherishOS](https://github.com/NFS86/Rom-Builder/actions/workflows/cherish.yml/badge.svg)](https://github.com/NFS86/Rom-Builder/actions/workflows/cherish.yml)
---
+++ cherish.sh
@@ -0,0 +1,15 @@
+# sync rom
+repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/NFS-projects/local_manifest --depth 1 -b cherish-12 .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
+# build rom
+source build/envsetup.sh
+lunch cherish_rosy-userdebug
+export TZ=Asia/Jakarta
+export USE_NFSparts=true
+export NFS_BUILD_VARIANT=normal
+curl -s -X POST "https://api.telegram.org/bot${tg_token}/sendMessage" -d chat_id="${tg_id}" -d "disable_web_page_preview=true" -d "parse_mode=html" -d text="===================================%0A<b>$device</b> Starting Building Rom%0A<b>ROM:</b>$rom_name%0A<b>Build Type:</b>$NFS_BUILD_VARIANT%0A$(echo "${var_cache_report_config}")"
+brunch rosy
+
+# end
