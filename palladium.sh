c47173d3a99e2e5e0f47e621473fd0f609d74900
NFS86 <jarbull86@gmail.com>
2022-03-27 15:08:07 +0000

    Gas vip build

--- palladium.sh
+++ palladium.sh
@@ -8,7 +8,7 @@ source build/envsetup.sh
 lunch palladium_rosy-userdebug
 export TZ=Asia/Jakarta
 export USE_NFSparts=true
-export NFS_BUILD_VARIANT=normal
+export NFS_BUILD_VARIANT=overclock
 export USE_REMOVE_PACKAGES=true
 export NFS_BUILD_ROM=${rom_name}
 curl -s -X POST "https://api.telegram.org/bot${tg_token}/sendMessage" -d chat_id="${tg_id}" -d "disable_web_page_preview=true" -d "parse_mode=html" -d text="===================================%0A<b>$device</b> Starting Building Rom%0A<b>ROM:</b>$rom_name%0A<b>Build Type:</b>$NFS_BUILD_VARIANT%0A$(echo "${var_cache_report_config}")"
