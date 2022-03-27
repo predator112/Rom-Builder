281b093a437496896ab1a79e20c43088be90b5fe
NFS86 <jarbull86@gmail.com>
2022-03-27 10:13:53 +0000

    Ngelu
    Signed-off-by: NFS86 <jarbull86@gmail.com>

--- palladium.sh
+++ palladium.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Palladium-OS/platform_manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/NFS-projects/local_manifest --depth 1 -b pd .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j4
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
@@ -12,6 +12,6 @@ export NFS_BUILD_VARIANT=normal
 export USE_REMOVE_PACKAGES=true
 export NFS_BUILD_ROM=${rom_name}
 curl -s -X POST "https://api.telegram.org/bot${tg_token}/sendMessage" -d chat_id="${tg_id}" -d "disable_web_page_preview=true" -d "parse_mode=html" -d text="===================================%0A<b>$device</b> Starting Building Rom%0A<b>ROM:</b>$rom_name%0A<b>Build Type:</b>$NFS_BUILD_VARIANT%0A$(echo "${var_cache_report_config}")"
-mka palladium
+mka palladium -j8
 
 # end
