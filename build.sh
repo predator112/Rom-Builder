71c1cf902d1e01873e72d7b98685e876502ab1fe
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2022-03-30 17:08:37 +0700

    Drop more export

--- build.sh
+++ build.sh
@@ -9,9 +9,6 @@ export TZ=Asia/Jakarta
 export USE_NFSparts=true
 export NFS_BUILD_VARIANT=overclock
 export USE_ARCANA_PACKAGE=yes
-export LINUX_COMPILE_BY=medusa
-export LINUX_COMPILE_HOST=nfs-project
-export SELINUX_IGNORE_NEVERALLOWS=true
 lunch aosp_rosy-userdebug
 curl -s -X POST "https://api.telegram.org/bot${tg_token}/sendMessage" -d chat_id="${tg_id}" -d "disable_web_page_preview=true" -d "parse_mode=html" -d text="===================================%0A<b>$device</b> Starting Building Rom%0A<b>ROM:</b>$rom_name%0A<b>Build Type:</b>$NFS_BUILD_VARIANT%0A$(echo "${var_cache_report_config}")"
 make bacon -j8
