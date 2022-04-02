76a381d8c4fa0df426edf37b59032eff9f17583d
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2022-04-02 14:40:07 +0700

    CherishOS

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/NFS-projects/local_manifest --depth 1 -b PE-12 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b twelve-one -g default,-mips,-darwin,-notdefault
+git clone https://github.com/NFS-projects/local_manifest --depth 1 -b cherish-12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -8,9 +8,9 @@ export NFS_BUILD_ROM_NAME=${rom_name}
 source build/envsetup.sh
 export TZ=Asia/Jakarta
 export USE_NFSparts=true
-export NFS_BUILD_VARIANT=overclock
-lunch aosp_rosy-userdebug
+export NFS_BUILD_VARIANT=normal
+lunch cherish_rosy-userdebug
 curl -s -X POST "https://api.telegram.org/bot${tg_token}/sendMessage" -d chat_id="${tg_id}" -d "disable_web_page_preview=true" -d "parse_mode=html" -d text="===================================%0A<b>$device</b> Starting Building Rom%0A<b>ROM:</b>$rom_name%0A<b>Build Type:</b>$NFS_BUILD_VARIANT%0A$(echo "${var_cache_report_config}")"
-mka bacon
+brunch rosy
 
 # end
