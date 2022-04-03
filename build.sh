84a852b86cdda2b94b774ded112e1371faeb2f09
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2022-04-03 21:13:10 +0700

    CipherOS vip Build

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ export NFS_BUILD_ROM_NAME=${rom_name}
 source build/envsetup.sh
 export TZ=Asia/Jakarta
 export USE_NFSparts=true
-export NFS_BUILD_VARIANT=normal
+export NFS_BUILD_VARIANT=overclock
 lunch cipher_rosy-userdebug
 curl -s -X POST "https://api.telegram.org/bot${tg_token}/sendMessage" -d chat_id="${tg_id}" -d "disable_web_page_preview=true" -d "parse_mode=html" -d text="===================================%0A<b>Redmi 5</b> Starting Building Rom%0A<b>ROM:</b>$rom_name%0A<b>Build Type:</b>$NFS_BUILD_VARIANT%0A$(echo "${var_cache_report_config}")"
 make bacon
