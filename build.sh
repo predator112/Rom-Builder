afdb077e6b47fbedee8d2f557989f68b27ab215d
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2022-04-05 13:59:14 +0700

    Test build witch coreGAPPS

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ source build/envsetup.sh
 export TZ=Asia/Jakarta
 export USE_NFSparts=true # this is for NFSDev™ only
 export NFS_BUILD_VARIANT=normal # this is for NFSDev™ only
+export NFS_BUILD_TYPE_GAPPS=coregapps # this is for NFSDev™ only
 lunch awaken_rosy-userdebug
 curl -s -X POST "https://api.telegram.org/bot${tg_token}/sendMessage" -d chat_id="${tg_id}" -d "disable_web_page_preview=true" -d "parse_mode=html" -d text="===================================%0A<b>${device_model}</b> Starting Building Rom%0A<b>ROM:</b>$rom_name%0A<b>Build Type:</b>$NFS_BUILD_VARIANT%0A$(echo "${var_cache_report_config}")"
 make bacon
