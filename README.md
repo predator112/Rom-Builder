eb10ead8fc5b67259df8b6a7bc76f3232d412092
NFS86 <jarbull86@gmail.com>
2022-04-06 00:36:53 +0700

    add juice config build [skip ci]
    Signed-off-by: NFS86 <jarbull86@gmail.com>

--- README.md
+++
@@ -1,5 +0,0 @@
-# Change this from workflows file yml
-* device="rosy"
-* git_name="NFS86"
-* git_repo="Rom-Builder"
-* git_branch="master"
--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/NFS-projects/local_manifest --depth 1 -b 12.1 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b eleven-plus -g default,-mips,-darwin,-notdefault
+git clone https://github.com/c3eru/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 export TZ=Asia/Jakarta
-lunch awaken_rosy-userdebug
+export BUILD_USERNAME=mobxprjkt
+export BUILD_HOSTNAME=RANDOMBUILD
+lunch aosp_juice-userdebug
 curl -s -X POST "https://api.telegram.org/bot${tg_token}/sendMessage" -d chat_id="${tg_id}" -d "disable_web_page_preview=true" -d "parse_mode=html" -d text="===================================%0A<b>${device_model}</b> Starting Building Rom%0A<b>ROM:</b>$rom_name%0A$(echo "${var_cache_report_config}")"
-make bacon
+mka bacon
 # end
