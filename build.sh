178db5f4a44aa262bf4f00086cf146657142d5af
predator112 <65210380+predator112@users.noreply.github.com>
2022-04-06 10:31:27 +0700

    TEST fix build

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
 # build rom
 . build/envsetup.sh
-# lunch aosp_whyred-userdebug
+lunch havoc_whyred-userdebug
 curl -s -X POST "https://api.telegram.org/bot${tg_token}/sendMessage" -d chat_id="${tg_id}" -d "disable_web_page_preview=true" -d "parse_mode=html" -d text="===================================%0A<b>${device_model}</b> Starting Building Rom%0A<b>ROM:</b>$rom_name%0A<b>Build Type:</b>$NFS_BUILD_VARIANT%0A$(echo "${var_cache_report_config}")"
 # make xd -j8
 brunch
